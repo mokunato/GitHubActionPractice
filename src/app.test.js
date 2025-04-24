0447cbe8e85ad6f5f1c48aa4c29c443a7ddfe329
mokunato <mokunato@gmail.com>
2025-04-24 21:26:41 +0700

    update app.js and app.test.js 2

--- src/app.test.js
+++ src/app.test.js
@@ -1,5 +1,5 @@
 const { dayOfTheWeek } = require('./app');
-
+const { monthOfTheYear } = require('./app');
 
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
@@ -7,5 +7,5 @@ test('getDay returns the long-format day of the week', () => {
 });
 test('getDay returns the month of the year', () => {
     const month = monthOfTheYear( new Date('3/11/2020') );
-    expect( month ).toBe('July');
+    expect( month ).toBe('November');
 }); 
